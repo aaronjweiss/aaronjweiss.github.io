72238af6f6e34e3c00d23b8eb09a7216f340e2b9
aaronjweiss <aiceweiss@hotmail.com>
2017-08-17 13:43:52 -0400

    Pong Rotated Correctly

--- pong.js
+++ pong.js
@@ -129,8 +129,8 @@ Player.prototype.update = function () {
 function Ball(x, y) {
     this.x = x;
     this.y = y;
-    this.x_speed = 0;
-    this.y_speed = 3;
+    this.x_speed = 3;
+    this.y_speed = 0;
 }
 
 Ball.prototype.render = function () {
@@ -148,25 +148,24 @@ Ball.prototype.update = function (paddle1, paddle2) {
     var bottom_x = this.x + 5;
     var bottom_y = this.y + 5;
 
-    if (this.x - 5 < 0) {
-        this.x = 5;
-        this.x_speed = -this.x_speed;
-    } else if (this.x + 5 > width) {
-        this.x = width-5;
-        this.x_speed = -this.x_speed;
+    if (this.y - 5 < 0) {
+        this.y = 5;
+        this.y_speed = -this.y_speed;
+    } else if (this.y + 5 > height) {
+        this.y = height-5;
+        this.y_speed = -this.y_speed;
     }
 
-    if (this.y < 0 || this.y > height) {
-        this.x_speed = 0;
-        this.y_speed = 3;
+    if (this.x < 0 || this.x > width) {
+        this.x_speed = 3;
+        this.y_speed = 0;
         this.x = width/2;
         this.y = height/2;
     }
 
-    if (top_y > height/2) {
+    /*if (top_y > height/2) {
         if (top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
-            this.y_speed = -3;
-			this.y_speed = -this.y_speed - Math.random();
+            this.y_speed = -3;			
             this.x_speed += (Math.random()*2);
             this.y += this.y_speed;
         }
@@ -175,6 +174,20 @@ Ball.prototype.update = function (paddle1, paddle2) {
             this.y_speed = 3;
             this.x_speed += (Math.random()*2);
             this.y += this.y_speed;
+        }
+    }*/
+	
+	if (top_x > width/2) {
+        if (top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x && top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y) {
+            this.x_speed = -3;			
+            this.y_speed += (Math.random()*2);
+            this.x += this.x_speed;
+        }
+    } else {
+        if (top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x && top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y) {
+            this.x_speed = 3;
+            this.y_speed += (Math.random()*2);
+            this.x += this.x_speed;
         }
     }
 };
