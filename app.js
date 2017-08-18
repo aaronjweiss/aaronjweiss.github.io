a8a0702fc98ceb546cd20eeb5db9868c85d43797
aaronjweiss <aiceweiss@hotmail.com>
2017-08-18 15:13:50 -0400

    about

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
 
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('about', {
+			url: '/about',
+            templateUrl: 'about.html' //for web use
             // we'll get to this in a bit       
         });
 
